4522a1d7a0188815cf7bf3ffeb906ab6c3a03757
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-08-28 13:46:27 -0400

    Added icons from bootstrap and a breadcrumb nav bar up top. Also a warning alert above the date picker.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import PhotoCard from './components/PhotoCard';
 import Vote from './components/Vote';
 import DatePicker from './components/DatePicker'
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Breadcrumb tag="nav" listTag="div">
+        <BreadcrumbItem tag="a" href="#">Home</BreadcrumbItem>
+        <BreadcrumbItem tag="a" href="https://www.nasa.gov/">Nasa</BreadcrumbItem>
+        <BreadcrumbItem tag="a" href="https://api.nasa.gov/">Data</BreadcrumbItem>
+        <BreadcrumbItem tag="a" href="https://lambdaschool.com/">Lambda</BreadcrumbItem>
+      </Breadcrumb>
       <PhotoCard />
       <div className="com">
         <DatePicker />
--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Alert } from 'reactstrap';
 
 export default function DatePicker(props){
 
     return (
         <div className="theDate">
+            <Alert>The Date Picker is currently down!</Alert>
             <h2>Pick A Date</h2>
             <label>Date: <input id="date" type="date"></input></label>
         </div>
